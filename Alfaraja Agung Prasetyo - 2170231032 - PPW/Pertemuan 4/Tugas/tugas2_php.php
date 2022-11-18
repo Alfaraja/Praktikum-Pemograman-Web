86463d279d0adfb7f39f5d8531854185cba9f675
Alfaraja <118531893+Alfaraja@users.noreply.github.com>
2022-11-18 18:47:49 +0700

    Add files via upload

---
+++ Alfaraja Agung Prasetyo - 2170231032 - PPW/Pertemuan 4/Tugas/tugas2_php.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>Tugas 2 PHP</title>
+
+</head>
+
+<body>
+    <h1>
+        <center>Biodata Alfaraja Agung Prasetyo</center>
+    </h1>
+    <br>
+    <?php
+    $nama = "Alfaraja Agung Prasetyo";
+    $nim = "2170231032";
+    $jur = "Teknik Informatika";
+    $jen = "S1";
+    $umr = "20 Tahun";
+    $askot = "Bekasi , Indonesia";
+
+    echo "<center>Nama : $nama</center><br><br>";
+    echo "<center>NIM : $nim</center><br><br>";
+    echo "<center>Jurusan : $jur</center><br><br>";
+    echo "<center>Jenjang : $jen</center><br><br>";
+    echo "<center>Umur : $umr</center><br><br>";
+    echo "<center>Asal Kota : $askot</center>";
+    ?>
+</body>
+
+</html>
